fe27985724ceaf406aba18e97e741d03b6e8221c
anthonywong0206-commits <anthony.wong0206@gmail.com>
2026-05-25 21:10:32 +0800

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
-  theme: { extend: { fontFamily: { sans: ['Inter','Noto Sans TC','system-ui','sans-serif'] } } },
-  plugins: []
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: { extend: {} },
+  plugins: [],
 }
